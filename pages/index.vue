f116db6af031f1e64654350081c498b08cbd3cbf
Ayon Garcia <ayon.geo@gmail.com>
2022-10-12 19:41:55 -0300

    V1.0.0

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+    
+</template>
+
+<script>
+    export default{
+        middleware: "authenticated",
+        mounted(){
+            $nuxt.$router.push("/dashboard");
+        }
+    }
+</script>
